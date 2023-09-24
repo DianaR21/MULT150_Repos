0b4409432df27395569fa772f0d3db5a81151d20
DianaR21 <drodriguez005@student.sdccd.edu>
2023-09-23 22:51:41 -0700

    TryItYourself and Hour7

---
+++ drodriguez_Hour7/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,14 @@
+using Unityengine;
+public class HelloWorldScript : Monobehaviour
+{
+    //Use this for initialization
+    void start ()
+    {
+        print ("Hello World");
+
+    }
+    //Update is called once per frame
+    void update ()
+    {
+      }
+}
